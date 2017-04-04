2f0ef1bc8d6e7da4244bb7221a3f78a5c545f646
luxxcn <953817@qq.com>
2017-04-04 09:04:11 +0800

    字符串MD5加密

---
+++ IOS/ioslibs/ioslibs/ObjectC-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  ObjectC-Bridge.h
+//  ioslibs
+//
+//  Created by 星 鲁 on 2017/4/4.
+//  Copyright © 2017年 xxing. All rights reserved.
+//
+
+#ifndef ObjectC_Bridging_Header_h
+#define ObjectC_Bridging_Header_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* ObjectC_Bridge_h */
